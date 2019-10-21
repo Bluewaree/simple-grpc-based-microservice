06d268001e2f18b5d38aec737d86acd13f06af2a
blue <belkihelmehdi2@gmail.com>
2019-10-21 20:41:35 +0100

    feat: add the grpc server

---
+++ server.py
@@ -0,0 +1,35 @@
+from concurrent import futures
+import threading
+import time
+import grpc
+import consumption_data_pb2
+import consumption_data_pb2_grpc
+import csv
+
+class Listener(consumption_data_pb2_grpc.ConsumptionDataServiceServicer):
+
+    def ConsumptionData(self, request, context):
+        data = []
+        with open('meterusage.csv', 'r') as csvFile:
+            reader = csv.reader(csvFile)
+            for row in reader:
+                data.append({"date":row[0],"consumption":row[1]})
+
+        return consumption_data_pb2.ReturnData(data=data)
+
+
+def serve():
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=1))
+    consumption_data_pb2_grpc.add_ConsumptionDataServiceServicer_to_server(Listener(), server)
+    server.add_insecure_port("[::]:9999")
+    server.start()
+    try:
+        while True:
+            print("Server Running...")
+            time.sleep(10)
+    except KeyboardInterrupt:
+        server.stop(0)
+
+
+if __name__ == "__main__":
+    serve()
