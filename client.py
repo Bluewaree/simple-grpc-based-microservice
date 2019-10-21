8a0804813546851b36b6137c5c29e07862e51586
blue <belkihelmehdi2@gmail.com>
2019-10-21 21:13:06 +0100

    feat: add an api endpoint to grpc client that returns json object of the message

--- client.py
+++ client.py
@@ -3,19 +3,22 @@ import time
 import grpc
 import consumption_data_pb2
 import consumption_data_pb2_grpc
+from flask import Flask
+from google.protobuf.json_format import MessageToJson 
+app = Flask(__name__)
+ 
+@app.route('/', methods=["GET"])
+def index():
+    response = run()
+    return MessageToJson(response)
 
 def run():
     with grpc.insecure_channel("localhost:9999") as channel:
         stub = consumption_data_pb2_grpc.ConsumptionDataServiceStub(channel)
         response = stub.ConsumptionData(consumption_data_pb2.RequestData())
-        for item in response.data:
-            print(item.date,item.consumption)
         channel.unsubscribe(close)
-        exit()
+        return response
 
 
 def close(channel):    
     channel.close()
-
-if __name__ == "__main__":
-    run()
